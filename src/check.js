dc2cfbc20552a63d051b0b57eadfe302bd6d9601
Pavlo Cherevach <pf4pad.fr@gmail.com>
2022-01-21 13:26:03 +0200

    check

--- src/check.js
+++ src/check.js
@@ -1,21 +1,23 @@
-module.exports = function check(str, config) {
+const config = [['(', ')']];
+str = '())(';
+
+function check(str, config) {
   let count = 0;
   for (let i = 0; i < config.length; i++) {
     for (let j = 0; j < config[i].length; j++) {
       str.split('').forEach(el => {
         if (el === config[i][0]) {
           count++
-          if (count > 1) return false
+          console.log(count)
         } else if (el === config[i][1]) {
           count--
-          if (count < 0) return false
+          if (count === -1) return false
         }
+
       })
     }
   }
 
-
-  return true
+  return count !== 0
 }
-
-
+console.log(check(str, config))
