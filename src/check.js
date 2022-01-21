05ff2762f0fd44743ef2426a97a9e4969f282ca2
Pavlo Cherevach <pf4pad.fr@gmail.com>
2022-01-21 15:51:11 +0200

    check

--- src/check.js
+++ src/check.js
@@ -1,23 +1,37 @@
-const config = [['(', ')']];
-str = '())(';
+const config = [['|', '|']];;
+str = '||';
 
 function check(str, config) {
   let count = 0;
+  let c = 0
   for (let i = 0; i < config.length; i++) {
+    // if (count < 0) {
+    //   return false
+    // }
+
     for (let j = 0; j < config[i].length; j++) {
+
+
+
       str.split('').forEach(el => {
         if (el === config[i][0]) {
           count++
-          console.log(count)
+          // console.log(count, el)
         } else if (el === config[i][1]) {
           count--
-          if (count === -1) return false
+          c = count
         }
-
       })
+
+
     }
+    console.log(c)
+  }
+  if (c !== 0) {
+    return false
+  } else {
+    return true
   }
 
-  return count !== 0
 }
 console.log(check(str, config))
--- src/index.js
+++ src/index.js
@@ -1,21 +1,33 @@
 module.exports = function check(str, config) {
   let count = 0;
+  let c = 0
   for (let i = 0; i < config.length; i++) {
+
+
     for (let j = 0; j < config[i].length; j++) {
+
+
+
       str.split('').forEach(el => {
         if (el === config[i][0]) {
           count++
-          if (count > 1) return false
+
         } else if (el === config[i][1]) {
           count--
-          if (count < 0) return false
+          c = count
         }
       })
+
+
     }
+
+  }
+  if (c !== 0) {
+    return false
+  } else {
+    return true
   }
 
-
-  return true
 }
 
 
